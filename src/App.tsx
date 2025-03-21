ff575f2134b7d30303d21d574247524590e0160b
Epsilonely <yoongyum@kakao.com>
2025-03-22 00:41:58 +0900

    chore: Change grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import Block, { BlockType } from "./block/Block";
-
-
+import Block, { BlockType } from './block/Block';
 
 function App() {
   return (
-    <div className="grid grid-cols-4 grid-rows-2 gap-0 w-full h-screen bg-[#FF0000]/15 p-4">
-        <Block type={BlockType.BLOCK_TY_MARKET}/>
-        <Block />
-        <Block />
-        <Block />
-        <Block type={BlockType.BLOCK_TY_MARKET}/>
-        <Block />
-        <Block />
-        <Block />
+    <div className="grid grid-cols-3 grid-rows-2 gap-0 w-full h-screen bg-[#FF0000]/15 p-4">
+      <Block type={BlockType.BLOCK_TY_MARKET} />
+      <Block />
+      <Block />
+      <Block />
+      <Block />
+      <Block />
     </div>
   );
 }
