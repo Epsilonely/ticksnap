a70c0e5d248a1c2866fd56fd98c4e9165580898d
Epsilonely <yoongyum@kakao.com>
2025-03-23 00:58:44 +0900

    feat: Add TickerData #3
    - Tkcer Api 호출시 불러오는 데이터 추가

--- src/services/UpbitApi.ts
+++ src/services/UpbitApi.ts
@@ -6,10 +6,15 @@ interface MarketData {
 }
 
 interface TickerData {
-  market: string;
-  trade_price: number;
+  market: string; // 종목 구분 코드
+  trade_price: number; // 현재가
+  prev_closing_price: number; // 전일 종가(UTC 0시 기준)
+  change: string; // EVEN: 보합, RISE: 상승, FALL:하락
+  change_price: number; // 변화액의 절대값
+  change_rate: number; // 변화율의 절대값
   signed_change_rate: number;
-  acc_trade_price_24h: number;
+  acc_trade_price: number; // 누적 거래대금 (UTC 0시 기준)
+  acc_trade_price_24h: number; // 최근 24시간 동안의 총 거래대금
 }
 
 export const fetchMarkets = async () => {
