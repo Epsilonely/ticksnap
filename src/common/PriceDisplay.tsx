825abb8852753be1b96908e43f3b66efb729d1ef
Epsilonely <yoongyum@kakao.com>
2025-10-09 17:06:45 +0900

    chore: uiux

--- src/common/PriceDisplay.tsx
+++ src/common/PriceDisplay.tsx
@@ -5,14 +5,17 @@ interface PriceDisplayProps {
 }
 
 function PriceDisplay({ price, className = 'text-[#4C4C57]', decimalPlaces = 2 }: PriceDisplayProps) {
-  const formattedPrice = price.toFixed(decimalPlaces).replace(/(\.\d*?)0+$/, '$1').replace(/\.$/, '');
+  const formattedPrice = price
+    .toFixed(decimalPlaces)
+    .replace(/(\.\d*?)0+$/, '$1')
+    .replace(/\.$/, '');
   const [integerPart, decimalPart] = formattedPrice.split('.');
   const hasDecimal = decimalPart !== undefined;
 
   return (
     <span className={`${className}`}>
       <span>{Number(integerPart).toLocaleString()}</span>
-      {hasDecimal && <span className="opacity-70 text-[0.9em]">.{decimalPart}</span>}
+      {hasDecimal && <span className="opacity-70 text-[0.96em]">.{decimalPart}</span>}
     </span>
   );
 }
